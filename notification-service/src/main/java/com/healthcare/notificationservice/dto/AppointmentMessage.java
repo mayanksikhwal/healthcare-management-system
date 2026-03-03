2f62a184c4c32f7ef99a423f1edf2d476c357de2
Mayank Sikhwal <sikhwalmayank251@gmail.com>
2026-03-03 10:40:46 +0530

    Complete notification-service deployment

--- notification-service/src/main/java/com/healthcare/notificationservice/dto/AppointmentMessage.java
+++ notification-service/src/main/java/com/healthcare/notificationservice/dto/AppointmentMessage.java
@@ -1,8 +1,7 @@
 package com.healthcare.notificationservice.dto;
 
 import lombok.*;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 
 @Data
@@ -13,7 +12,7 @@ public class AppointmentMessage {
     private String patientEmail;
     private String doctorEmail;
 
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime appointmentDateTime;
 
     private String reason;
