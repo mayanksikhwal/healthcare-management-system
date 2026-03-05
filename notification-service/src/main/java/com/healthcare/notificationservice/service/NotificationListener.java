852a53625e3bd13368e3a677c757fee1dfcff59e
Mayank Sikhwal <sikhwalmayank251@gmail.com>
2026-03-06 02:23:49 +0530

    Imported some requirements in notification-service

--- notification-service/src/main/java/com/healthcare/notificationservice/service/NotificationListener.java
+++ notification-service/src/main/java/com/healthcare/notificationservice/service/NotificationListener.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+import org.springframework.context.annotation.Profile;
 
 @Service
 @Profile("local")
